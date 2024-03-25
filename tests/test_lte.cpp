6a2cb6e978fd2823e0bd059bd840d2f6e4cf60d9
cmo <christopher.osborne@glasgow.ac.uk>
2024-03-25 18:05:18 +0000

    Adding missing test file

---
+++ tests/test_lte.cpp
@@ -0,0 +1,9 @@
+#include "catch_amalgamated.hpp"
+#include "Populations.hpp"
+#include "Types.hpp"
+
+using Catch::Matchers::WithinRel;
+using Catch::Matchers::WithinAbs;
+
+TEST_CASE( "Test LTE", "[lte_pops]" ) {
+}
