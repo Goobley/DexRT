0c5a386a2178fb0d0b3d429970e406fe5a53cc6d
cmo <christopher.osborne@glasgow.ac.uk>
2024-08-16 16:16:23 +0100

    Tonemap fixes

--- cococo_lyb.py
+++ cococo_lyb.py
@@ -52,12 +52,20 @@ def tonemap(c, mode='aces', Gamma=2.2, bias=None):
         # https://github.com/TheRealMJP/BakingLab/blob/master/BakingLab/ACES.hlsl
         # https://knarkowicz.wordpress.com/2016/01/06/aces-filmic-tone-mapping-curve/
         # https://therealmjp.github.io/posts/sg-series-part-6-step-into-the-baking-lab/
-        acesIn = np.array([[0.59719, 0.35458, 0.04823],
-                            [0.07600, 0.90834, 0.01566],
-                            [0.02840, 0.13383, 0.83777]])
-        acesOut = np.array([[1.60475, -0.53108, -0.07367],
-                            [-0.10208,  1.10813, -0.00605],
-                            [-0.00327, -0.07276,  1.07602]])
+        acesIn = np.ascontiguousarray(
+            np.array([
+                [0.59719, 0.35458, 0.04823],
+                [0.07600, 0.90834, 0.01566],
+                [0.02840, 0.13383, 0.83777]
+            ]).T
+        )
+        acesOut = np.ascontiguousarray(
+            np.array([
+                [1.60475, -0.53108, -0.07367],
+                [-0.10208,  1.10813, -0.00605],
+                [-0.00327, -0.07276,  1.07602]]
+            ).T
+        )
         if bias is None:
             bias = 0.8
 
