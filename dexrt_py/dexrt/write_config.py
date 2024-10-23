0087fc14ef21ea976f1736af0efd8952a4c9989f
cmo <christopher.osborne@glasgow.ac.uk>
2024-10-23 11:31:59 +0100

    Remove dexrt_py to make submodule

--- dexrt_py/dexrt/write_config.py
+++
@@ -1,7 +0,0 @@
-from dexrt_py.dexrt.config_schemas.dexrt import DexrtNonLteConfig, DexrtLteConfig, DexrtGivenFsConfig
-import yaml
-
-def write_config(conf, path="dexrt.yaml"):
-    data = conf.model_dump()
-    with open(path, "w") as f:
-        yaml.dump(data, f)
