b95a3b9a864129d59dc219630cedb620bee05630
cmo <christopher.osborne@glasgow.ac.uk>
2025-04-01 11:47:13 +0100

    Adding updated ducky build scripts

--- build/cmake_wild-duck_nompi.sh
+++ build/cmake_wild-duck_nompi.sh
@@ -1,30 +1,22 @@
 #!/bin/bash
 
-source setup_wild-duck_env.sh
 GCC_INCLUDE_PATH=$(g++ --print-file-name=include)
-MAGMA_BASE_PATH=$(spack location -i magma)
-MAGMA_INCLUDE_PATH="${MAGMA_BASE_PATH}/include/"
-MAGMA_LIB_PATH="${MAGMA_BASE_PATH}/lib/"
-MAGMA_LIB="-lmagma"
 
 ./cmake_clean.sh
 
-      # -DYAKL_CUDA_FLAGS="-O3 --ptxas-options=-v" \
+      # -DYAKL_CUDA_FLAGS="-O3 --ptxas-options=-v -ftz=true" \
       # -DYAKL_DEBUG="On"               \
       # -DYAKL_B4B="On"                  \
-cmake -DCMAKE_CXX_COMPILER=g++     \
-      -DCMAKE_CUDA_COMPILER=nvcc    \
-      -DCMAKE_C_COMPILER=gcc        \
-      -DCMAKE_Fortran_COMPILER=gfortran \
-      -DYAKL_ARCH="CUDA"              \
-      -DYAKL_AUTO_PROFILE="On"             \
-      -DYAKL_CUDA_FLAGS="-O3 --generate-line-info" \
-      -DYAKL_INT64_RESHAPE="On"       \
-      -DDEXRT_CUDA_ARCHITECTURES="86" \
+      # -DYAKL_VERBOSE="On" \
+cmake  \
+      -DDEX_ARCH="CUDA"              \
+      -DYAKL_AUTO_PROFILE="On"         \
+      -DDEX_CXX_FLAGS="-O3 --generate-line-info -ftz=true -std=c++20" \
       -DGCC_INCLUDE_PATH="${GCC_INCLUDE_PATH}" \
-      -DMPI_INCLUDE_PATH="" \
-      -DMAGMA_INCLUDE_PATH="${MAGMA_INCLUDE_PATH}" \
       -DNETCDF_INCLUDE_PATH="$(nc-config --includedir)" \
-      -DLDLIBS="$(nc-config --libs) -L${MAGMA_LIB_PATH} ${MAGMA_LIB}" \
-      -DLDFLAGS="-Wl,-rpath,${MAGMA_LIB_PATH}:$(nc-config --libdir)" \
+      -DLDLIBS="$(nc-config --libs)" \
+      -DLDFLAGS="-Wl,-rpath,$(nc-config --libdir)" \
+      -DKokkos_ROOT="$(pwd)/../kokkos/" \
+      -DCMAKE_PREFIX_PATH="$(pwd)/../kokkos/lib64/cmake;$(pwd)/../kokkos-kernels/lib64/cmake/" \
+      -DCMAKE_BUILD_TYPE="Release" \
       ..
